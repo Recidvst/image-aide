c9166bd830fb2559b5f33af7d446668969900c0a
Chris Snowden <cdsnowden@gmail.com>
2021-02-23 16:56:56 +0000

    Update isValidURL.js

--- client/util/isValidURL.js
+++ client/util/isValidURL.js
@@ -1,5 +1,6 @@
 export default function(str) {
   // eslint-disable-next-line
   var res = str.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
+  // TODO: review alternate match regex : /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g;
   return (res !== null);
 }
